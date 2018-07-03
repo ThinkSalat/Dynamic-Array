6640fc3d529bc3339ad420974621aebd439ee58f
Shawn Salat <shawn.salat@gmail.com>
2018-07-02 20:05:32 -0700

    throw errors when accessing indexes that don't exist on array

--- lib/dynamic_array.rb
+++ lib/dynamic_array.rb
@@ -4,7 +4,7 @@ class DynamicArray
   attr_reader :length
 
   def initialize
-    @arr = StaticArray.new(0)
+    @arr = StaticArray.new(4)
   end
 
   def length
@@ -20,24 +20,36 @@ class DynamicArray
 
   # O(1)
   def [](index)
-    raise Exception("index out of bounds")
+    # raise Exception,.new("index out of bounds") unless length > index
+    raise Exception,  "index out of bounds" unless length > index
+    @arr[index]
   end
 
   # O(1)
   def []=(index, value)
+    # raise Exception,.new("index out of bounds") unless length > index
+    raise Expection "index out of bounds" unless length > index
+    @arr[index] = value
   end
 
   # O(1)
   def pop
+    raise Exception, "index out of bounds" if length == 0
+    result = @arr[length-1]
+    @arr[length-1] = nil
+    result
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if @arr[length-1]
+    @arr[length] = val
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise Exception, "index out of bounds" 
   end
 
   # O(n): has to shift over all the elements.
@@ -53,7 +65,12 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
-
-    StaticArray.new(length*2)
+    new_arr = StaticArray.new(length*2)
+    i = 0
+    while i < length 
+      new_arr[i] = @arr[i]
+      i += 1
+    end
+    return new_arr
   end
 end
